069eb82a20e791f085bddeedc395a6803eb23b7c
Lypef <lypef@live.com>
2017-12-16 11:07:18 -0600

    Todo listo, solo falta caller id

---
+++ CallerMe/iTextSharp/text/Color.vb
@@ -0,0 +1,11 @@
+﻿Imports iTextSharp.text
+
+Namespace iTextSharp.text
+    Friend Class Color
+        Inherits BaseColor
+
+        Public Sub New(red As Integer, green As Integer, blue As Integer)
+            MyBase.New(red, green, blue)
+        End Sub
+    End Class
+End Namespace
