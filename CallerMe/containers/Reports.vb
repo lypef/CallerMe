f6330ebf400c185395622546b5fcd74dd0ef90ef
lypef <lypef@live.com>
2019-03-04 18:03:55 -0600

    Nuevos botones

--- CallerMe/containers/Reports.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Reports
-
-    Private Sub Reports_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Icon = System.Drawing.Icon.FromHandle(My.Resources.ico.GetHicon())
-        Me.WindowState = FormWindowState.Maximized
-    End Sub
-End Class
